86b3f2a7d191547f2b73ca463645b678d6cd8cf6
Mariola Pasko <paskomariola@gmail.com>
2020-07-03 11:55:56 +0200

    countdown days

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
 
 const currentYear = new Date().getFullYear();
-const newYearTime = new Date(`January 01 ${currentYear +1} 00:00:00`);
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+function updateCountdown() {
+    const currentTime = new.Date();
+    const difference = newYearTime - currentTime;
+
+    const days = Math.floor(difference / 1000 / 60 / 60 / 24);
+    
+}
+
+updateCountdown()
