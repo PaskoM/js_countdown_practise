c234621e841fc7e3b74beb092a26c9af81da5d15
Mariola Pasko <paskomariola@gmail.com>
2020-07-03 11:35:30 +0200

    countdown

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
 
 const currentYear = new Date().getFullYear();
-const newYearTime = new Date('January 01 2021 00:00:00');
+const newYearTime = new Date(`January 01 ${currentYear +1} 00:00:00`);
